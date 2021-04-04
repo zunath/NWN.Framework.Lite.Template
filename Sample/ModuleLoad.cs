9c6130d145388b3bb5b3eb1f64d3d96587ae8e3d
Scott Milliorn <scottmilliorn@gmail.com>
2021-04-03 19:03:58 -0700

    x2_mod_def_load

---
+++ Sample/ModuleLoad.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NWN.Framework.Lite;
+
+namespace Sample
+{
+    public class ModuleLoad
+    {
+        // This method will be run whenever the script "x2_mod_def_load" is run. 
+        // In our example module, this happens when the server finishes loading the module file.
+        // Script names must adhere to the NWN restrictions (alphanumeric with some special characters and no longer than 16 characters)
+        // The method name is arbitrary and can be called whatever you want.
+        // Methods must be public and static so that the framework can pick them up when the module loads.
+        [ScriptHandler ("x2_mod_def_load")]
+        public static void OnModuleLoad() => Console.WriteLine($"x2_mod_def_load launched at {DateTime.Now}.");
+    }
+}
